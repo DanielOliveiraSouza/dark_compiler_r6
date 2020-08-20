85268d9497e48ed520cd1e5e12e2148a8c1d70be
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2020-08-19 21:01:09 -0300

    move *.txt para testes/

--- run.sh
+++ run.sh
@@ -2,11 +2,8 @@
 rm *.{o,exe}
 make
 #for i in $(ls *.txt ) do 
-./dark_cc.exe input  teste.asm 
-nasm -felf64 teste.asm -o teste.o;
-clang teste.o -o teste.exe;
-./teste.exe
-./dark_cc.exe input-2.txt teste2.asm
-nasm -felf64 teste2.asm -o teste2.o;
-gcc teste2.o -o teste2.exe -no-pie
-./teste2.exe
+./dark_cc.exe testes/input  teste.asm 
+./output.exe
+./dark_cc.exe testes/input-2.txt teste2.asm
+./output.exe
+
